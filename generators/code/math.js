c0ef4b064cad4f0bbb82cecd3f13aff54c0c7fea
halu <151787619+halufun@users.noreply.github.com>
2025-06-02 11:15:30 -0700

    restructure generators

--- generators/code/math.js
+++ generators/code/math.js
@@ -8,26 +8,38 @@ export function init() {
         },
         oplist: ["*","+","/","-","^","^/","%"],
         complist: ["==",">","<","<=",">="],
-        speclist: ["log","-log","sin","cos","tan","cot","sec","csc","abs","floor","ceiling","round"]
+        speclist: ["log","-log","sin","cos","tan","cot","sec","csc","abs","floor","ceiling","round"],
         blocks: {
-            text: "%input1 %input2 %input3",
-            function: "factor",
-            inputs: {
-              input1: "Number",
-              operator: "inlist",
-              input2: "Number"
-            }
-        },
-    }
-}
-export function factor(args) {
-    if (args.operator !== "^/" && args.operator !== "^") {
-        return `(${args.input1}${args.operator}${args.input2})`
-    } else {
-        if (args.operator == "^") {
-            return `(Math.pow(${args.input1},${args.input2}))`
-        } else {
-            return `(Math.pow(${input1}, 1/${input2}))`
+            operator: {
+                text: "%input1 %operator %input2",
+                generate: function(args) {
+                    if (args.operator !== "^/" && args.operator !== "^") {
+                        return `(${args.input1}${args.operator}${args.input2})`;
+                    } else {
+                        if (args.operator == "^") {
+                            return `(Math.pow(${args.input1},${args.input2}))`;
+                        } else {
+                            return `(Math.pow(${args.input1}, 1/${args.input2}))`;
+                        }
+                    }
+                },
+                inputs: {
+                    input1: "Number",
+                    operator: "oplist",
+                    input2: "Number"
+                }
+            },
+            comparator: {
+                text: "%input1 %comparator %input2",
+                generate: function(args) {
+                    return `(${args.input1}${args.comparator}${args.input2}`;
+                },
+                inputs: {
+                    input1: "Number",
+                    comparator: "complist",
+                    input2: "Number"
+                }
+            },
         }
-    }
+    };
 }
