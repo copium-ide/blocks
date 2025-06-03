516415b3198acea350047f744106bf417abd875a
halu <151787619+halufun@users.noreply.github.com>
2025-06-03 09:45:38 -0700

    fixed imports to new api
    the entirety of the extension is now loaded into a local object, yipee!

--- generators/code/code.js
+++ generators/code/code.js
@@ -4,36 +4,27 @@ let modulePaths = [
     "./files.js",
     "./os.js"
 ];
+let modules = {};
 
-// USAGE: updateImports("path");
 export function updateImports(path) {
     if (!modulePaths.includes(path)) {
         modulePaths.push(path);
     }
-    console.log("Updated imports:", modulePaths);
+    importModules();
 }
 
 export async function importModules() {
     for (let i = 0; i < modulePaths.length; i++) {
         try {
             const mod = await import(modulePaths[i]);
-            console.log(`Imported ${modulePaths[i]}`, mod);
+            const author = mod.main.info.author;
+            const namespace = mod.main.info.namespace;
+            if (!modules.hasOwnProperty(author)) {
+                modules[author] = {}; 
+            }
+            modules[author][namespace] = mod.main;
         } catch (error) {
             console.error(`Error importing module ${modulePaths[i]}:`, error);
         }
     }
-}
-
-// Example usage
-importModules();
-
-
-
-let context = {
-    paths: {},
-    elements: {},
-};
-
-export function getContext() {
-    return context;
 }
