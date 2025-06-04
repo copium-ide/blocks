b7da22997885e761286f5516792194e43e343bbc
halu <151787619+halufun@users.noreply.github.com>
2025-06-04 10:14:53 -0700

    Create project.js

---
+++ generators/code/project.js
@@ -0,0 +1,40 @@
+export const data = {
+  meta: {
+    copium: "Copium-Lite",
+    author: "copium-ide",
+    name: "Test Project",
+    namespace: "testproj"
+  },
+  modules: [
+    "./default/math.js",
+    "./default/variables.js"
+  ],
+  project: {
+    assets: {},
+    code: {
+      script1: {
+        block1: {
+          block: "copium-ide.vars.make",
+          inputs: {
+          type: {value: "String"},
+          name: {value: "Bingus"},
+          value: {value: " says hello"}
+          }
+        },
+        block2: {
+          block: "copium-ide.vars.make",
+          inputs: {
+            type: {value: "String"},
+            name: {value: "Boingus"},
+            value: {
+              block: "copium-ide.vars.get",
+              inputs: {
+                name: {value: "Bingus"}
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
