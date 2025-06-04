f8c00d1a0f501462222472b50abeb721e6bc339c
halu <151787619+halufun@users.noreply.github.com>
2025-06-04 09:28:34 -0700

    fixed syntax

--- generators/code/default/variables.js
+++ generators/code/default/variables.js
@@ -8,7 +8,10 @@ export const main = {
         website: "https://github.com/copium-ide",
         description: "Make, get, and modify variables."
     },
-    init: function() {return ``;}, // Return code to be added at the beginning of the script.
+    init: function() {
+        return `
+Copium.env.vars = {};`.;
+    }, // Return code to be added at the beginning of the script.
     
     blocks: {
         make: {
@@ -26,7 +29,7 @@ export const main = {
         get: {
             text: "variable %name",
             generate: function(args) {
-                return `(Copium.env.vars.${args.name})`;
+                return `(Copium.env.vars.${args.name}.value)`;
             },
             inputs: {
                 name: "String"
