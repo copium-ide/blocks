689234acff79eb0b17e7b4f58989eba9a57ed322
halu <151787619+halufun@users.noreply.github.com>
2025-06-04 10:06:37 -0700

    added project importing

--- generators/code/code.js
+++ generators/code/code.js
@@ -1,15 +1,21 @@
 // updateImports() is used to add to this list. It then imports said modules and creates objects for them
-const modulePaths = [
-    "./default/math.js",
-    "./default/variables.js"
-];
+export const modulePaths = [];
 export const modules = {};
+export const project = {};
+
+export function importProject(path) {
+    const proj = await import(path);
+    project = proj.data;
+    modulePaths = [];
+    for (let i = 0; i < project.modules.length; i++) {
+        updateImports(project.modules[i]);
+        importModules();
+    }
 
 export function updateImports(path) {
     if (!modulePaths.includes(path)) {
         modulePaths.push(path);
     }
-    importModules();
 }
 
 export async function importModules() {
