934c740e45e7d06d5cd5bf874590613943b762f1
halu <151787619+halufun@users.noreply.github.com>
2025-06-11 12:54:43 -0700

    Create console.js

---
+++ generators/code/default/console.js
@@ -0,0 +1,27 @@
+export const main = {
+    info: {
+        author: "copium-ide",
+        namespace: "console",
+        name: "Console",
+        version: "1.0.0",
+        email: "",
+        website: "https://github.com/copium-ide",
+        description: "Simple console logging."
+    },
+    init: function() {
+        return ``;
+    },
+    
+    blocks: {
+        make: {
+            text: "%type to console %input",
+            generate: function(args) {
+                return `console[${args.type}](${args.input})`
+            },
+            inputs: {
+                type: ["log","warn","error"],
+                input: "String"
+            }
+        },
+    }
+};
