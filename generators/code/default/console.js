f8b566c485c13d02f049cca6b981d9403710d327
halu <151787619+halufun@users.noreply.github.com>
2025-06-12 10:14:18 -0700

    replace " with `

--- generators/code/default/console.js
+++ generators/code/default/console.js
@@ -1,12 +1,12 @@
 export const main = {
     info: {
-        author: "copium-ide",
-        namespace: "console",
-        name: "Console",
-        version: "1.0.0",
-        email: "",
-        website: "https://github.com/copium-ide",
-        description: "Simple console logging."
+        author: `copium-ide`,
+        namespace: `console`,
+        name: `Console`,
+        version: `1.0.0`,
+        email: ``,
+        website: `https://github.com/copium-ide`,
+        description: `Simple console logging.`
     },
     init: function() {
         return ``;
@@ -14,13 +14,13 @@ export const main = {
     
     blocks: {
         push: {
-            text: "%type to console %input",
+            text: `%type to console %input`,
             generate: function(args) {
                 return `console[${args.type}](${args.input})`
             },
             inputs: {
-                type: ["log","warn","error"],
-                input: "String"
+                type: [`log`,`warn`,`error`],
+                input: `String`
             }
         },
     }
