43b3746d9028533da37eeea28da0dc8a3d1b2f18
halu <151787619+halufun@users.noreply.github.com>
2025-06-12 10:14:58 -0700

    replace " with `

--- generators/code/project.js
+++ generators/code/project.js
@@ -1,60 +1,60 @@
 export const data = {
   meta: {
-    copium: "Copium-Lite",
-    author: "copium-ide",
-    name: "Test Project",
-    namespace: "testproj"
+    copium: `Copium-Lite`,
+    author: `copium-ide`,
+    name: `Test Project`,
+    namespace: `testproj`
   },
   modules: [
-    "https://cdn.jsdelivr.net/gh/copium-ide/blocks/generators/code/default/math.js",
-    "https://cdn.jsdelivr.net/gh/copium-ide/blocks/generators/code/default/variables.js",
-    "https://cdn.jsdelivr.net/gh/copium-ide/blocks/generators/code/default/console.js"
+    `https://cdn.jsdelivr.net/gh/copium-ide/blocks/generators/code/default/math.js`,
+    `https://cdn.jsdelivr.net/gh/copium-ide/blocks/generators/code/default/variables.js`,
+    `https://cdn.jsdelivr.net/gh/copium-ide/blocks/generators/code/default/console.js`
   ],
   project: {
     assets: {},
     code: [
       {
-        name: "Script1",
+        name: `Script1`,
         code: [
           {
-            block: "copium-ide.vars.make",
+            block: `copium-ide.vars.make`,
             inputs: {
-            type: {value: "Number"},
-            name: {value: "Bingus"},
+            type: {value: `Number`},
+            name: {value: `Bingus`},
             value: {value: 1}
             }
           },
           {
-            block: "copium-ide.vars.make",
+            block: `copium-ide.vars.make`,
             inputs: {
-              type: {value: "Number"},
-              name: {value: "Boingus"},
+              type: {value: `Number`},
+              name: {value: `Boingus`},
               value: {
-                block: "copium-ide.vars.get",
+                block: `copium-ide.vars.get`,
                 inputs: {
-                  name: {value: "Bingus"}
+                  name: {value: `Bingus`}
                 }
               }
             }
           },
           {
-            block: "copium-ide.console.push",
+            block: `copium-ide.console.push`,
             inputs: {
-              type: {value: "log"},
+              type: {value: `log`},
               input: {
-                block: "copium-ide.math.operator",
+                block: `copium-ide.math.operator`,
                 inputs: {
                   input1: {
-                    block: "copium-ide.vars.get",
+                    block: `copium-ide.vars.get`,
                     inputs: {
-                      name: {value: "Bingus"}
+                      name: {value: `Bingus`}
                     }
                   },
-                  operator: {value: "+"},
+                  operator: {value: `+`},
                   input2: {
-                    block: "copium-ide.vars.get",
+                    block: `copium-ide.vars.get`,
                     inputs: {
-                      name: {value: "Boingus"}
+                      name: {value: `Boingus`}
                     }
                   },
                 }
