313f18145f40093a11ce703245cc834868059ba8
halufun <frogillius@hotmail.com>
2025-06-30 16:15:31 -0700

    added local webserver stuff

---
+++ run.py
@@ -0,0 +1,10 @@
+from http.server import SimpleHTTPRequestHandler, HTTPServer
+
+def run(server_class=HTTPServer, handler_class=SimpleHTTPRequestHandler, port=8000):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print(f"Serving HTTP on port {port} (http://localhost:{port}/) ...")
+    httpd.serve_forever()
+
+if __name__ == '__main__':
+    run()
