66167e92679f6e67d5eaee029a371f88f72bee4a
halufun <frogillius@hotmail.com>
2025-07-30 06:40:17 -0700

    supprt custom types

--- generators/shape/blocks.js
+++ generators/shape/blocks.js
@@ -2,29 +2,19 @@ import * as blocks from './blockComponents.js';
 import * as inputs from './inputComponents.js';
 
 export function Block(type, colors, sizes) {
-    switch (type) {
-        //---------------------------------------------------------BLOCKS
-        case 'block':
-            return blocks.branch(colors, sizes, 'notch', 'notch');
-        case 'hat':
-            return blocks.branch(colors, sizes, 'hat', 'notch');
-        case 'end':
-            return blocks.branch(colors, sizes, 'notch', 'flat');
-        //---------------------------------------------------------INPUTS
-        case 'label':
-            return inputs.input(colors, sizes, 'label');
-        case 'number':
-            return inputs.input(colors, sizes, 'number');
-        case 'string':
-            return inputs.input(colors, sizes, 'string');
-        case 'boolean':
-            return inputs.input(colors, sizes, 'boolean');
-        case 'array':
-            return inputs.input(colors, sizes, 'array');
-        case 'object':
-            return inputs.input(colors, sizes, 'object');
-        default:
-            console.warn("Block type not recognized:", type);
-            return { points: [], snapPoints: [] }; // Return a valid empty shape
-    }
+    if (type != 'block' && type != 'hat' && type != 'end') {
+        return inputs.input(colors, sizes, type);
+    } else if (type === 'block' || type === 'hat' || type === 'end') {
+        switch (type) {
+            case 'block':
+                return blocks.branch(colors, sizes, 'notch', 'notch');
+            case 'hat':
+                return blocks.branch(colors, sizes, 'hat', 'notch');
+            case 'end':
+                return blocks.branch(colors, sizes, 'notch', 'flat');
+        }
+    } else if (typeof type === 'function') {
+        return type(colors, sizes);
+    } else {
+        console.error("Unknown block type:", type);}
 }
